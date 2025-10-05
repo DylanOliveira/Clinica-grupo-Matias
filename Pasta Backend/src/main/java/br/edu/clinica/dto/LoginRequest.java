f8c1180b514c0a269e069a8ec0e39d5c4bb86319
Dylan Oliver <167258746+DylanOliveira@users.noreply.github.com>
2025-10-05 16:42:32 -0300

    Create LoginRequest.java

---
+++ Pasta Backend/src/main/java/br/edu/clinica/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package dto;
+
+public class LoginRequest {
+    private String email;
+    private String senha; 
+    
+    public String getEmail(){ return email; }
+    public void setEmail(String e){ this.email = e; }
+    public String getSenha(){ return senha; }
+    public void setSenha(String s){ this.senha = s; }
+}
