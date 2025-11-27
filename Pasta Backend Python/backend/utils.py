d05faf78a8605aedaa5c6207c5e2abe5a6b2ec14
Dylan Oliver <167258746+DylanOliveira@users.noreply.github.com>
2025-11-27 07:31:09 -0300

    Criar utils.py

---
+++ Pasta Backend Python/backend/utils.py
@@ -0,0 +1,16 @@
+from flask_jwt_extended import get_jwt_identity
+from functools import wraps
+from flask import jsonify
+from models import Usuario
+
+def admin_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        identity = get_jwt_identity()
+        if not identity:
+            return jsonify({"msg":"Token inválido"}), 401
+        user = Usuario.query.filter_by(id=identity).first()
+        if not user or user.tipo != 'admin':
+            return jsonify({"msg":"Ação permitida somente para admin"}), 403
+        return fn(*args, **kwargs)
+    return wrapper
